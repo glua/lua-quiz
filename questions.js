0eb3219e36f31e46d7b89b3ed5d68cd2c3296700
Lennart Bernhardt <l.bernhardt@live.de>
2016-10-31 14:56:12 +0100

    Update questions.js

--- questions.js
+++ questions.js
@@ -49,5 +49,17 @@ window.QuestionList = [
         ],
         correctFeedback: "Correct!",
         wrongFeedback: "HOW COULD YOU NOT KNOW THIS?!?!?"
+    },
+  {
+        question: "What isn't a table in Lua?",
+        correctAnswer: 4,
+        options: [
+            "An array",
+            "{}",
+            "A string",
+            "Nothing"
+        ],
+        correctFeedback: "You now understand the deepest thruth that lies in lua!",
+        wrongFeedback: "John Lua is not amused"
     }
 ];
